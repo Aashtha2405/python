28d2bd390b111c7c6d522c1747b9b5dc3f0d243f
Aashtha Kumari <202515293+Aashtha2405@users.noreply.github.com>
2025-07-13 13:29:03 +0530

    Add files via upload

---
+++ 04practise.py
@@ -0,0 +1,27 @@
+#problem1
+fruits = []
+f1 = input("Enter fruit name: ")
+fruits.append(f1)
+f2 = input("Enter fruit name: ")
+fruits.append(f2)
+f3 = input("Enter fruit name: ")
+fruits.append(f3)
+f4 = input("Enter fruit name: ")
+fruits.append(f4)
+f5 = input("Enter fruit name: ")
+fruits.append(f5)
+f6 = input("Enter fruit name: ")
+fruits.append(f6)
+f7 = input("Enter fruit name: ")
+fruits.append(f7)
+
+print(fruits)
+
+#problem2
+l = [2,7,8,9]
+print(sum(l))
+
+#problem3
+a = (7,0,8,0,0,9)
+n = a.count(0)
+print(n)
