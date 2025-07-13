59e9f3f7bfa01516dcafb9e5383ff49f7c2abbdd
Aashtha Kumari <202515293+Aashtha2405@users.noreply.github.com>
2025-07-13 13:24:28 +0530

    Add files via upload

---
+++ 01module.py
@@ -0,0 +1,54 @@
+import pyjokes
+
+joke = pyjokes.get_joke()
+print(joke)
+
+# # This is a module that provides a function to get a joke
+
+
+##problem1
+# print('''Twinkle twinkle little star.
+# How I wonder what you are.
+# Up above the world so high.
+# Like a diamond in the sky.
+# Twinkle twinkle little star.
+# How I wonder what you are.
+# Twinkle twinkle little star.
+# How I wonder what you are.
+# Up above the world so high.
+# Like a diamond in the sky.
+# Twinkle twinkle little star.
+# How I wonder what you are.''')
+
+
+##problem2 use repl and print table of 5
+# normally print 5*1=5 and so on
+
+
+##problem3
+# import pyttsx3
+# engine = pyttsx3.init()
+# engine.say("I will speak this text")
+# engine.runAndWait()
+
+
+
+# #problem4
+# # write a program to print contents of a dictionary using the os module. search online for the function which used that
+# import os
+
+# # Sample dictionary
+# my_dict = {
+#     "name": "Aashtha",
+#     "branch": "CSE",
+#     "year": "1st"
+# }
+
+# # Using os module to print current working directory first
+# print("Current Working Directory:", os.getcwd())
+
+# # Printing contents of dictionary
+# print("Contents of the dictionary:")
+# for key, value in my_dict.items():
+#     print(key, ":", value)
+
