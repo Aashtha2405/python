e878b11eb432ac5c3b698f78da1e7f9df7783c44
Aashtha Kumari <202515293+Aashtha2405@users.noreply.github.com>
2025-07-13 13:26:15 +0530

    Add files via upload

---
+++ 02type.py
@@ -0,0 +1,21 @@
+a = 31
+t =  type(a) #class <int>
+print(t)
+
+a = 7.7
+t = type(a) 
+print(t)
+
+a = "Aastha"
+t = type(a)
+print(t)
+
+a = "77.7"
+t = type(a)
+print(t) #yha double quote ke andar number v string hota h
+
+#ab convert karenge usko float me
+a = "32.2"
+b = float(a)
+t = type(b)
+print(t)
