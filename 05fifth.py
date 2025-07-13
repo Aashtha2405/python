83c47a372377e6c13d08ce5cee8e73fb7fdef07a
Aashtha Kumari <202515293+Aashtha2405@users.noreply.github.com>
2025-07-13 13:29:25 +0530

    Add files via upload

---
+++ 05fifth.py
@@ -0,0 +1,26 @@
+# #dictionary is a collection of key-value pairs
+# #key is unique and immutable
+# #value can be mutable or immutable
+# #dictionary is unordered,mutable,no dublicate and indexed
+# #dictionary is defined by curly braces{}
+
+marks = {
+    "aastha": 100,
+    "sita": 90,
+    "rumi": 80,
+    "ram": 70
+}
+print(marks,type(marks))
+
+# #dictionary methods
+marks = {
+    "aastha": 100,
+    "sita": 90,
+    "rumi": 80,
+    "ram": 70
+}
+print(marks,type(marks))
+print(marks.keys())  # returns keys of dictionary
+print(marks.values()) # returns values of dictionary
+print(marks.items()) # return key-value pairs of dictionary
+
