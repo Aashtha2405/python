a4c23cc5baf14b9c7d5f1f8053a96db9a3f79b44
Aashtha Kumari <202515293+Aashtha2405@users.noreply.github.com>
2025-07-13 13:27:00 +0530

    Add files via upload

---
+++ 02practise.py
@@ -0,0 +1,30 @@
+#problem1
+a=1
+b=7
+print(a+b)
+
+#problem2
+a=34
+b=3
+print("remainder when a is divided by b is", a % b)
+
+#problem3
+#str
+a = input("enter the value of a: ")
+print(type(a))
+#integer
+a = int(input("enter the value of a: "))
+print(type(a))
+
+#problem4
+a = 5>7
+print(a)
+
+#problem5
+a = int(input("enter a number 1: "))
+b = int(input("enter the number 2: " ))
+print("the avg of two number is",(a+b)/2)
+
+#problem6
+a = int(input("enrt your number : "))
+print("the square of given number is ",a**2)
