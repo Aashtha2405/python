85ca04a4155ae770eaa2254012b257ac22c8fae7
Aashtha Kumari <202515293+Aashtha2405@users.noreply.github.com>
2025-07-13 13:25:39 +0530

    Add files via upload

---
+++ 02datatypes.py
@@ -0,0 +1,57 @@
+# a = 1 # a is an integer
+# b = 5.22 # b is a floating point number
+# c = "Aastha" # c is a string
+# d = False #d is boolean variable
+# e = None #e is a none type variable
+
+
+# #rule of variable
+# aaa = 435 # valid variable
+# a_b_c = 45 #valid variable
+# aastha = 5125 #valid variable
+
+# @aastha = 34 invalid variable
+# h@rry # invallid variable
+
+#operators
+
+#arithmetic operator
+a = 7
+b = 3
+c = a + b
+print(c)
+
+#Assignment operator
+a = 4-2 # assign 4-2 in a 
+print(a)
+b = 6
+b +=3 #increment the value of b by 3 and then assign it to b 
+print(b)
+
+#comparison operator
+d = 5>4
+print(d) #always give a boolean value
+d = 5==5
+print(d)
+d = 5>=5
+print(d)
+
+#logical operators
+e = True or False
+print(e)
+
+#truth table of 'or'
+print("True or False is ", True or False)
+print("True or True is ", True or True)
+print("False or True is ", False or True)
+print("False or False is ", False or False)
+
+#truth table of 'and'
+print("True or False is ", True and False)
+print("True or True is ", True and True)
+print("False or True is ", False and True)
+print("False or False is ", False and False)
+
+#NOT operator
+print(not(True))
+print(not(False))
