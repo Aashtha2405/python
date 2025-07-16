942c8b988007b689de6cb01e4c95d620df412ba7
Aashtha Kumari <202515293+Aashtha2405@users.noreply.github.com>
2025-07-16 09:32:21 +0530

    Add files via upload

---
+++ 07seventh.py
@@ -0,0 +1,19 @@
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+
+# #the same task can be done like this
+# for i in range(1,6):
+#     print(i)
+
+# i = 1
+# while(i<6):
+#     print(i)
+#     i +=1
+
+# i = 1
+# while(i<16):
+#     print(i)
+#     i +=1
+
