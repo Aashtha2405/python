b0b3fa9636e07c7f4525e1d94d10c2b6004ab41f
Aashtha Kumari <202515293+Aashtha2405@users.noreply.github.com>
2025-07-22 20:08:45 +0530

    Add files via upload

---
+++ 10tenth.py
@@ -0,0 +1,12 @@
+#object oriented programming
+class employee:
+    name = "aastha"
+    language = "python"
+    salary = "10000000"
+
+    
+aastha = employee()
+print("emolyee details")
+print(aastha.name, aastha.language, aastha.salary)
+
+
